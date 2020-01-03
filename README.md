527e817a64302e6258af1e189fb5ab4767f4e6d8
Peedrog18 <peedrog18@gmail.com>
2020-01-03 19:36:29 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ysdhhb
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ysdhhb)
---
+++ index.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import Hello from './Hello';
+import './style.css';
+
+let nombre = "Uriel";
+
+
+function A(){
+  return <p>Hola {nombre} </p>;
+}
+function B(){
+  return <p>{nombre}: 10</p>;
+}
+class MiComponenteDeClase extend Component{
+  render(){
+    return <p>Hola soy de clase </p>
+  }
+}
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: 'React'
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        < A />
+        < B />
+      </div>
+    );
+  }
+}
+
+render(<App />, document.getElementById('root'));
